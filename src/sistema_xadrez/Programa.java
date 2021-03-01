93b0fbb064b9f9a268ecfa5fd262d6c03ae95927
Henrique Gonçalves Mendes <henrique19915@gmail.com>
2021-03-01 15:01:44 -0300

    Início do projeto

---
+++ src/sistema_xadrez/Programa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema_xadrez;
+
+/**
+ *
+ * @author Henrique
+ */
+public class Programa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Olá Mundo!");
+    }
+    
+}
